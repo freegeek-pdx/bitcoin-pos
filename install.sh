48bf8800d2998a623bd2ed909c4046d707c4198d
Ryan Niebur <ryanryan52@gmail.com>
2013-09-28 08:57:10 -0700

    Add initial configs and scripts

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git submodule update --init greenaddress-pos-tools/
+sudo apt-get install -y python-qrencode python-qt4 python-zmq python
+sudo apt-get install -y bitcoind # FG backports
