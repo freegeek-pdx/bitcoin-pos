43b9719ff8e8e93ac6e37c947a74979d308db26d
Ryan Niebur <ryanryan52@gmail.com>
2013-09-30 14:16:39 -0700

    add one more sudo

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ BASE=$(dirname $(readlink -f $0))
 
 git clone dev.freegeek.org:/git/greenaddress-pos-tools/ greenaddress-pos-tools/
 sudo apt-get install -y python-qrencode python-qt4 python-zmq python
-ln -sf $BASE/BitCoinPOS.desktop /usr/share/applications/
+sudo ln -sf $BASE/BitCoinPOS.desktop /usr/share/applications/
