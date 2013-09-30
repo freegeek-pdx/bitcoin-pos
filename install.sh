15cc9a475d68b664fed39031866f2e1976d1f1e4
Ryan Niebur <ryanryan52@gmail.com>
2013-09-29 20:49:36 -0700

    kill the underlying ssh process when exiting

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+BASE=$(dirname $(readlink -f $0))
+. $BASE/lib.sh
+
 git submodule update --init greenaddress-pos-tools/
 sudo apt-get install -y python-qrencode python-qt4 python-zmq python
-sudo apt-get install -y bitcoind # FG backports
+ln -sf $BASE/BitCoinPOS.desktop /usr/share/applications/
